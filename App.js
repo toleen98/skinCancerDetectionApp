d678c7ee0f98332df9f6c809dca9173eae8e0a10
esraashabrani <esraashabrani@gmail.com>
2020-07-18 10:45:13 +0300

    Implement patient sideMenu + Patient home screen to capture image

--- App.js
+++ App.js
@@ -1,22 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { Dimensions } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Skin Cancer Application Home Screen
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  HomeScreen,
+  ProfileScreen,
+  AppointmentsScreen,
+  ReportScreen,
+  AboutUsScreen,
+  HelpScreen,
+  SettingsScreen,
+  LogoutScreen
+} from "./src/components/screens";
+import HomePatient from './src/components/screens/homePatient'
+import Sidebar from './src/components/common/sideBar';
+import Header from './src/components/common/header';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const DrawerNavigator = createDrawerNavigator({
+  HomeScreen :{
+      screen:HomePatient,
+      navigationOptions: ({ navigation }) => ({     
+        title :'Home',
+        drawerIcon :({tintColor}) =><Feather name='home' size={16} color={tintColor}/>
+      })
+   
+      
   },
+  ProfileScreen:{
+    screen:ProfileScreen,
+    navigationOptions : {
+      title :'Profile',
+      drawerIcon :({tintColor}) =><Feather name='user' size={16} color={tintColor}/>
+    }
+  },
+  AppointmentsScreen :{
+    screen:AppointmentsScreen,
+    navigationOptions : {
+      title :'Appointments',
+      drawerIcon :({tintColor}) =><MaterialIcons name="date-range" size={16} color={tintColor} />
+    }
+  },
+  ReportScreen :{
+    screen:ReportScreen,
+    navigationOptions : {
+      title :'My Report',
+      drawerIcon :({tintColor}) =><MaterialIcons name="report" size={16} color={tintColor} />
+    }
+  },
+  AboutUsScreen :{
+    screen:AboutUsScreen,
+    navigationOptions : {
+      title :'About Us',
+      drawerIcon :({tintColor}) =><MaterialIcons name="info" size={16} color={tintColor} />
+    }
+  },
+  HelpScreen :{
+    screen:HelpScreen,
+    navigationOptions : {
+      title :'Help',
+      drawerIcon :({tintColor}) =><MaterialIcons name="help" size={16} color={tintColor} />
+    }
+  },
+  SettingsScreen :{
+    screen:SettingsScreen,
+    navigationOptions : {
+      title :'Settings',
+      drawerIcon :({tintColor}) =><MaterialIcons name="settings" size={16} color={tintColor} />
+    }
+  },
+  LogoutScreen :{
+      screen:LogoutScreen,
+    navigationOptions : {
+      title :'Logout',
+      drawerIcon :({tintColor}) =><MaterialIcons name="exit-to-app" size={16} color={tintColor} />
+    }
+  }
+},{
+  contentComponent: props => <Sidebar {...props}/>
 });
+
+export default createAppContainer(DrawerNavigator);
