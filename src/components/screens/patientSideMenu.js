07da3b39457d946ee608c471f41e004688a17c53
esraashabrani <esraashabrani@gmail.com>
2020-07-27 21:35:38 +0300

    refactor style the navigation drawer to add icons

--- src/components/screens/patientSideMenu.js
+++ src/components/screens/patientSideMenu.js
@@ -2,41 +2,112 @@ import "react-native-gesture-handler";
 import React from "react";
 //import { createAppContainer } from "react-navigation";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ImageBackground,
+  Image,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
-import { NavigationContainer,createAppContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  NavigationContainer,
+  createAppContainer,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import AsyncStorage from "@react-native-community/async-storage";
 
 import HomePatient from "./homePatient";
 import PatientUpdate from "./patientUpdate";
-import ViewDoctor from "./viewDoctor"
+import ViewDoctor from "./viewDoctor";
 import Logout from "./logOut";
 import AboutUs from "./aboutUs";
 import Help from "./help";
 
-const DrwerNavigator = createDrawerNavigator()
+const DrwerNavigator = createDrawerNavigator();
 const Stack = createStackNavigator();
-function Drwer (props) {
-  return(
+function Drwer(props) {
+  return (
     <NavigationContainer>
-     <DrwerNavigator.Navigator>
-
-       <DrwerNavigator.Screen  name="Home" component={HomePatient}/>
-       <DrwerNavigator.Screen  name="Profile" component={PatientUpdate}/>
-       <DrwerNavigator.Screen  name="Doctors" component={ViewDoctor}/>
-       <DrwerNavigator.Screen  name="Appointments" component={HomePatient}/>
-       <DrwerNavigator.Screen  name="My Report" component={HomePatient}/>
-       <DrwerNavigator.Screen  name="Help" component={Help}/>
-       <DrwerNavigator.Screen  name="AboutUs" component={AboutUs}/>
-       <DrwerNavigator.Screen  name="Logout" component={Logout}/>
-     </DrwerNavigator.Navigator>
-   </NavigationContainer>
-  )
-
+      <DrwerNavigator.Navigator>
+        <DrwerNavigator.Screen
+          name="Home"
+          component={HomePatient}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <Feather name="home" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="Profile"
+          component={PatientUpdate}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <Feather name="user" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="Doctors"
+          component={ViewDoctor}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <Fontisto name="doctor" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="Appointments"
+          component={HomePatient}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <MaterialIcons name="date-range" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="My Report"
+          component={HomePatient}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <MaterialIcons name="report" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="Help"
+          component={Help}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <MaterialIcons name="help" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="AboutUs"
+          component={AboutUs}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <MaterialIcons name="info" size={16} color={tintColor} />
+            ),
+          }}
+        />
+        <DrwerNavigator.Screen
+          name="Logout"
+          component={Logout}
+          options={{
+            drawerIcon: ({ tintColor }) => (
+              <MaterialIcons name="exit-to-app" size={16} color={tintColor} />
+            ),
+          }}
+        />
+      </DrwerNavigator.Navigator>
+    </NavigationContainer>
+  );
 }
-
 
 export default Drwer;
