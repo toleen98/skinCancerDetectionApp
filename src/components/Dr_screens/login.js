1686e52a22ebcd78c6d73cb5972dcae2dc67f4f3
toleen98 <toleenballool98@gmail.com>
2020-08-07 22:01:00 +0300

    refactor login doctor screen style

--- src/components/Dr_screens/login.js
+++ src/components/Dr_screens/login.js
@@ -6,8 +6,10 @@ import {
   StyleSheet,
   Platform,
   View,
+  ImageBackground
 } from "react-native";
 import { Block, Button, Input, NavBar, Text } from "galio-framework";
+import {  MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
 import theme from "../../theme";
 import { Image } from "react-native";
@@ -50,9 +52,10 @@ function LoginDr(props) {
       .post("https://skincancerbackend.herokuapp.com/api/user/doctor/login", user)
       .then((res) => {
         console.log(res.data.doctor._id);
-        id = res.data.doctor._id;
-        AsyncStorage.setItem("access_token", JSON.stringify(id));
+       
         if (res.data.result === true) {
+          id = res.data.doctor._id;
+          AsyncStorage.setItem("access_token", JSON.stringify(id));
           alert("Login Successed! ");
           Actions.push("HomeDr");
         } else if (res.data === false) {
@@ -66,28 +69,29 @@ function LoginDr(props) {
 
   return (
     <Block safe flex style={{ backgroundColor: theme.COLORS.WHITE }}>
+    <ImageBackground 
+    source={require('../../../assets/register-bg.png')}
+    style={{width: width, height: height}} 
+>  
       <KeyboardAvoidingView style={styles.container} behavior="height" enabled>
         <Block flex center style={{ marginTop: theme.SIZES.BASE }}>
           <Text> {"\n"}</Text>
           <Image source={require("../../../assets/splash.png")} />
-          <Text muted center size={theme.SIZES.FONT * 2} color={"#18DCFF"}>
+          <Text muted center size={theme.SIZES.FONT * 2} color={theme.COLORS.PRIMARY}>
             {" "}
             Login{" "}
           </Text>
         </Block>
-        <Block flex={2} center space="evenly">
-          <Block flex={2}>
-            <Text size={theme.SIZES.FONT * 0.875} color={"#18DCFF"}>
-              {" "}
-              Email{" "}
-            </Text>
+        <Block flex={1.5} center space="evenly" >
+          <Block flex={2} middle>
+         
             <Input
-              rounded
+              borderless
               id="email"
               type="email-address"
               placeholder="exmple@gmail.com"
               autoCapitalize="none"
-              style={{ width: width * 0.9 }}
+              style={{ width: width * 0.8 }}
               // onChangeText={text => this.handleChange('email', text)}
               // label='Email'
               onBlur={() => {
@@ -100,48 +104,71 @@ function LoginDr(props) {
               }}
               // value= {email}
               onChange={handleEmailChange}
+              iconContent={<MaterialCommunityIcons name="email" size={24} color={theme.COLORS.PRIMARY} style={{marginRight: 12}} />}
             />
-            <Text size={theme.SIZES.FONT * 0.875} color={"#18DCFF"}>
-              {" "}
-              Password{" "}
-            </Text>
+           
             <Input
-              rounded
+              borderless
               password
               viewPass
               id="password"
               placeholder="Password"
-              style={{ width: width * 0.9 }}
+              style={{ width: width * 0.8 }}
               // onChangeText={text => this.handleChange('password', text)}
               // value={password}
               onChange={handlePasswordChange}
+              iconContent={<Ionicons name="ios-lock" size={24} color={theme.COLORS.PRIMARY} style={{marginRight: 12}} />}
             />
             <Button
-              round
-              color={"#18DCFF"}
-              style={{ width: width * 0.9 }}
-              //onPress={() => navigation.navigate.push("MyDatePicker")}
-
-              //   onPress={() => props.navigation.navigate('MyDatePicker')}
+              
+              color={theme.COLORS.PRIMARY}
               onPress={onSubmit}
+              style={styles.createButton}
             >
-              Sign in
+              <Text bold size={14} color={theme.COLORS.WHITE}>Sign Up</Text>
             </Button>
+            <Block flex row >
+            <Text center color={'#8898AA'} size={theme.SIZES.FONT * 0.75}>
+                  {"Don't have an account? "}</Text>
+              <Button 
+              color="transparent" 
+              shadowless 
+              onPress={() => Actions.push('Signup')}
+              textStyle={{
+                color: theme.COLORS.PRIMARY,
+                fontSize: 12,
+                marginRight:125,
+                marginBottom:40
+                 }}
+              >
+                 Sign Up
+              </Button>
+            </Block>
           </Block>
         </Block>
       </KeyboardAvoidingView>
+      </ImageBackground>
     </Block>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-around",
-    paddingTop: theme.SIZES.BASE * 0.3,
-    paddingHorizontal: theme.SIZES.BASE,
-    backgroundColor: theme.COLORS.WHITE,
+    marginTop:100,
+    margin:20,
+    width: width * 0.9,
+    height: height * 0.80,
+    backgroundColor: "#F4F5F7",
+    borderRadius: 4,
+    shadowColor: theme.COLORS.BLACK,
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 8,
+    shadowOpacity: 0.1,
+    elevation: 1,
+    overflow: "hidden"
   },
   social: {
     width: theme.SIZES.BASE * 3.5,
@@ -149,6 +176,10 @@ const styles = StyleSheet.create({
     borderRadius: theme.SIZES.BASE * 1.75,
     justifyContent: "center",
   },
+  createButton: {
+    width: width * 0.5,
+    marginTop: 25
+  }
 });
 
 export default LoginDr;
